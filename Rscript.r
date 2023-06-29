2cb696dbbe03ffc364a79633608bfc69de2daec8
ScooLida <116261802+ScooLida@users.noreply.github.com>
2023-06-29 16:51:40 +0300

    Update Rscript.r

--- Rscript.r
+++ Rscript.r
@@ -2,7 +2,7 @@
 library (smartsnp, lib.loc = "~/_lib")
 
 My <- c(rep("A",2), rep("B",2))
-sm.pca <- smart_pca(snp_data = "1.traw", "2.traw", "3.traw", "SRR10011655.traw",
+sm.pca <- smart_pca(snp_data = "Z_filtered.traw",
                     missing_value = NA,
                         sample_group = My)
 pdf("my_plot.pdf")
