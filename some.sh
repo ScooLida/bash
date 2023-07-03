0689e595a130c27f655cbea40855803031c5cc36
ScooLida <116261802+ScooLida@users.noreply.github.com>
2023-07-03 20:19:46 +0300

    Update some.sh

--- some.sh
+++ some.sh
@@ -40,11 +40,12 @@ bowtie2-build reference_sequence.fasta index_name
 bowtie2 -p 6 -q --very-sensitive-local -x oc_index -U output_paired.pair1.truncated output_paired.pair2.truncated  -S lep.sam
 
 # форматирование samtools
-samtools view -bt oc_genome.fa.fai -o lep.bam lep.sam
+samtools faidx ref.fa
+samtools view -bt ~/cow/oc2_genome.fa.fai  -o lep.bam lep.sam
 samtools sort lep.bam -o lep_sort.bam
 samtools index lep_sort.bam
-bcftools mpileup -f oc_genome.fa lep_sort.bam | bcftools call -mv -Oz -o calls.vcf.gz
-
+bcftools mpileup -f oc2_genome.fa lep_sort.bam | bcftools call -mv -Oz -o calls.vcf.gz
+vcf-merge A.vcf.gz B.vcf.gz C.vcf.gz | bgzip -c > out.vcf.gz
 #фильтрация
 bcftools filter calls.vcf.gz -s LowQual -e 'QUAL<20 && DP<10' > filtered.vcf
 
@@ -59,15 +60,15 @@ samtools flagstat -@ 10 dna.bam > flagstat.txt
 
 
 
-bcftools mpileup -f oc_genome.fa SRR10011655.sort.bam  SRR11020300.sort.bam     SRR17908655.sort.bam     SRR5949623.sort.bam      SRR6485281.sort.bam      SRR17044867.sort.bam     SRR5949630.sort.bam      SRR6485284.sort.bam      SRR11020211.sort.bam     SRR17908654.sort.bam     SRR17908659.sort.bam     SRR17908658.sort.bam     SRR5949632.sort.bam  | bcftools call -mv -Oz -o calls.vcf.gz
+bcftools mpileup -f oc2_genome.fa SRR10011655.sort.bam  SRR11020300.sort.bam     SRR17908655.sort.bam     SRR5949623.sort.bam      SRR6485281.sort.bam      SRR17044867.sort.bam     SRR5949630.sort.bam      SRR6485284.sort.bam      SRR11020211.sort.bam     SRR17908654.sort.bam     SRR17908659.sort.bam     SRR17908658.sort.bam     SRR5949632.sort.bam  | bcftools call -mv -Oz -o calls2.vcf.gz
 
-samtools view -bt oc_genome.fa.fai -o z3.bam z3.sam
+samtools view -bt ~/cow/oc2_genome.fa.fai  -o z3.bam z3.sam
 samtools sort z3.bam -o z3_sort.bam
 export JAVA_HOME=/mss_users/ltursunova/jdk/jdk-17.0.7/
 export PATH="$JAVA_HOME/bin:$PATH"
-java -jar ~/picard/picard.jar MarkDuplicatesWithMateCigar REMOVE_DUPLICATES=true       I=z3_sort.bam       O=rd_z3.bam       M=mark_dups_w_mate_cig_metrics.txt
-samtools index rd_z3.bam
-samtools flagstat z3_sort.bam > flagstat.txt 
+java -jar ~/picard/picard.jar MarkDuplicatesWithMateCigar REMOVE_DUPLICATES=true       I=z3_sort.bam       O=rd_z3_sort.bam       M=mark_dups_w_mate_cig_metrics.txt
+samtools index rd_z3_sort.bam 
+samtools flagstat rd_z3_sort.bam  > rd_z3_sort.txt 
 samtools flagstat rd_z3.bam > flagstat.txt 
 
 cd ./TESTR
