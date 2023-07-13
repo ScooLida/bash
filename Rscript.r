b2379474ffc4647965dbe0fe825709308ef76eaf
ScooLida <116261802+ScooLida@users.noreply.github.com>
2023-07-13 17:55:13 +0300

    Update Rscript.r

--- Rscript.r
+++ Rscript.r
@@ -1,11 +1,17 @@
-
-library (smartsnp, lib.loc = "~/_lib")
-
-My <- c(rep("A",2), rep("B",2))
-sm.pca <- smart_pca(snp_data = "Z_filtered.traw",
-                    missing_value = NA,
-                        sample_group = My)
-pdf("my_plot.pdf")
-plot(sm.pca$pca.sample_coordinates[, c(3,4)])
-dev.off()
+# read in result files
+eigenValues <- read_delim("plinkPCA.eigenval", delim = " ", col_names = F)
+eigenVectors <- read_delim("plinkPCA.eigenvec", delim = " ", col_names = F)
  
+## Proportion of variation captured by each vector
+eigen_percent <- round((eigenValues / (sum(eigenValues))*100), 2)
+ 
+# PCA plot
+ggplot(data = eigenVectors) +
+  geom_point(mapping = aes(x = X3, y = X4, color = X1, shape = X1), size = 3, show.legend = TRUE ) +
+  geom_hline(yintercept = 0, linetype="dotted") +
+  geom_vline(xintercept = 0, linetype="dotted") +
+  labs(title = "PCA of selected goat populations",
+      x = paste0("Principal component 1 (",eigen_percent[1,1]," %)"),
+      y = paste0("Principal component 2 (",eigen_percent[2,1]," %)"),
+      colour = "Goat breeds", shape = "Goat breeds") +
+  theme_minimal()
