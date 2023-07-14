042cd0a5d09547b1d9db26523c4196d56e32cba2
ScooLida <116261802+ScooLida@users.noreply.github.com>
2023-07-14 12:23:39 +0300

    Update some.sh

--- some.sh
+++ some.sh
@@ -64,14 +64,18 @@ export PATH="$JAVA_HOME/bin:$PATH"
 java -jar ~/picard/picard.jar MarkDuplicatesWithMateCigar REMOVE_DUPLICATES=true       I=z5_sort.bam      O=rd_z5_sort.bam       M=mark_dups_w_mate_cig_metrics.txt
 samtools flagstat rd_z5_sort.bam > rd_z5_sort.txt
 
+[E::bgzf_read_block] Failed to read BGZF block data at offset 2240401020 expected 13782 bytes; hread returned 0
+[E::bgzf_read] Read block operation failed with error 4 after 30 of 39 bytes
 
 samtools quickcheck -qvvv z11207_2.bam 
-#z1_sort.bam has good EOF block.
 #rd_z1_sort.bam was missing EOF block when one should be present.
+#rd_z1_sort.bam has 9351 targets in header.
 #z11207_2.bam cannot be checked for EOF block because its filetype does not contain one.
-# bam  > bam
-# bam > sam > bam
 
+#z1_sort.bam has good EOF block.
+
+# bam > sam > bam
+# bam  > bam
 
 #соединяет vcf файлы
 tabix -p vcf zz1.vcf.gz
@@ -84,7 +88,7 @@ bcftools view  -M2 -v snps input.vcf.gz
 ~/plink --vcf out.vcf  --allow-extra-chr --make-bed --out  1307
 
 #выполняем pca
-~/plink --bfile 2all --pca 10 --allow-extra-chr --bad-freqs  --out pca2
+~/plink --bfile 2all --pca 4 --allow-extra-chr --bad-freqs  --out pca3
 
 ############################################################################################################
 for var in SRR10011655	SRR11020300	SRR17908655	SRR5949623	SRR6485281	SRR17044867	SRR5949630	SRR6485284	SRR11020211	SRR17908654	SRR17908659	SRR17908658	SRR5949632
