96fc2b2e05dfd7a4dec975792e3d26493d9194d5
ScooLida <116261802+ScooLida@users.noreply.github.com>
2024-02-11 20:44:17 +0300

    Update pca2.R

--- pca2.R
+++ pca2.R
@@ -1,24 +1,9 @@
-
-######################################
-# Principal Component Analysis - PCA #
-######################################
-
-## Genetic distances between individuals
-system("plink --cow --allow-no-sex --nonfounders --file ADAPTmap_TOP --distance-matrix --out dataForPCA")
-
-## Load data
-dist_populations<-read.table("dataForPCA.mdist",header=F)
-### Extract breed names
-fam <- data.frame(famids=read.table("dataForPCA.mdist.id")[,1])
-### Extract individual names 
-famInd <- data.frame(IID=read.table("dataForPCA.mdist.id")[,2])
-  
-## Perform PCA using the cmdscale function 
-# Time intensive step - takes a few minutes with the 4.5K animals
-mds_populations <- cmdscale(dist_populations,eig=T,5)
-
-## Extract the eigen vectors
-eigenvec_populations <- cbind(fam,famInd,mds_populations$points)
-
-## Proportion of variation captured by each eigen vector
-eigen_percent <- round(((mds_populations$eig)/sum(mds_populations$eig))*100,2)
+install.packages('vcfR') 
+install.packages('vegan')
+install.packages('ggplot2')
+install.packages('ggpubr')
+library(vcfR)    
+library(vegan)
+library(ggplot2)
+library(ggpubr)
+snps <- vcfR::read.vcfR("re.12.01.miss0.vcf", convertNA  = TRUE)
